0ad40f555fc659502c380336918e7a301fb68cc7
Ethan S <ethan.steip@gmail.com>
2022-03-28 21:33:17 -0400

    Implemented progress ring
    Built progress ring by following a youtube tutorial. The layout is still a little wonky, but the animation works great. Should be able to fix layout tomorrow.

--- resources/views/layouts/DashboardLayout.blade.php
+++ resources/views/layouts/DashboardLayout.blade.php
@@ -12,6 +12,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap" rel="stylesheet">
 
+    {{-- CSS --}}
+    <link rel="stylesheet" href="./css/progress-ring.css">
 
     @livewireStyles
     {{-- Alpine JS CDN --}}
--- resources/views/livewire/progress-component.blade.php
+++ resources/views/livewire/progress-component.blade.php
@@ -1,6 +1,6 @@
 
 <div class='flex w-1/2 gap-y-8 h-auto'>
-    <div class='flex flex-col w-full mt-8 mx-8 rounded-xl bg-white'>
+    <div class='flex flex-col w-full mt-8 mx-8 rounded-xl bg-gray-50'>
         
         <div class='flex w-full py-5'>
             <div class='flex gap-x-5 px-5'>
@@ -8,8 +8,27 @@
                 <button class='py-2 text-xl tracking-wide text-gray-600 font-medium border-b-2 border-white hover:border-blue-400 transition duration-300'>This Month</button>
             </div>
         </div>
-
-        <div class='h-48 w-48 rounded-full bg-green-200'>
+        
+        <div class='px-10'>
+            <div class='skill'>
+                <div class='outter'>
+                <div class='inner'>
+                    <div id="number" class='font-extrabold'>
+                    65%
+                    </div>
+                </div>
+                </div>
+                
+                <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="160px" height="160px">
+                <defs>
+                    <linearGradient id="GradientColor">
+                        <stop offset="0%" stop-color="#a5b4fc" />
+                        <stop offset="100%" stop-color="#3730a3" />
+                    </linearGradient>
+                </defs>
+                <circle cx="80" cy="80" r="70" stroke-linecap="round" />
+                </svg>
+            </div>
         </div>
 
 
